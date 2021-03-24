43e27ff4f9b85d4fbf5c28cece87f057627ea255
Marcelo Helio Garcia <marceloph2@yahoo.com.br>
2021-03-24 11:48:57 -0300

    Curso POO Java #13b​ - Polimorfismo Sobrecarga (Parte 2)

---
+++ src/aula013/Aula013.java
@@ -0,0 +1,16 @@
+package aula013;
+public class Aula013 {
+    public static void main(String[] args) {
+       //Mamifero x = new Mamifero();
+       //Lobo x = new Lobo();
+       Cachorro c = new Cachorro();
+       //c.emitirSom();
+       //c.reagir(true);
+       //c.reagir(false);
+       
+       c.reagir(2, 12.5f);
+       c.reagir(17, 4.5f);
+        
+    }
+    
+}
